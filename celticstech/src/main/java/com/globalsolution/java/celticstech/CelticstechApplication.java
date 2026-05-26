c42841037ea4cda958628c6dd664fad79fbabf46
Vinicius-R-Cardozo <virc06@gmail.com>
2026-05-26 19:11:07 -0300

    Adicionando projeto java na pasta

---
+++ celticstech/src/main/java/com/globalsolution/java/celticstech/CelticstechApplication.java
@@ -0,0 +1,13 @@
+package com.globalsolution.java.celticstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelticstechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelticstechApplication.class, args);
+	}
+
+}
---
+++ celticstech/src/test/java/com/globalsolution/java/celticstech/CelticstechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalsolution.java.celticstech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelticstechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
